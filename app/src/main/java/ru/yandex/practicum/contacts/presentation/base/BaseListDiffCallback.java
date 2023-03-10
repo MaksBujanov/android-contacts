275fb691fb10029866e9214aa0e7fec5c515d936
MaksBujanov <55047071+MaksBujanov@users.noreply.github.com>
2023-03-10 23:50:34 +0700

    10.03.2023 Применяем дженерики Спринт 4 - Тема 4 Проектная работа №2 - Урок 2 Применяем дженерики

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -4,16 +4,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
-public class BaseListDiffCallback<T extends ListDiffInterface> extends DiffUtil.ItemCallback<T> implements ListDiffInterface<T> {
+public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T>  {
 
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.theSameAs(newItem);
-    }
-
-    @Override
-    public boolean theSameAs(T element) {
-        return false;
     }
 
     @Override
